a11e9027156f5cfa0b2fae0b32fc1bc1ea303293
MrZzard <MrZzard@users.noreply.github.com>
2021-11-07 16:45:31 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.0508173f60c97ae819a1b22b3d38dca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8743d48045f3a585512e93bbe2f64633",
+    "url": "https://MrZzard.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "e571051e9a0c8cea87ab",
+    "url": "https://MrZzard.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ca8e32b3283780f51a45",
+    "url": "https://MrZzard.github.io/task-management-frontend/static/js/2.2bf3a042.chunk.js"
+  },
+  {
+    "revision": "e571051e9a0c8cea87ab",
+    "url": "https://MrZzard.github.io/task-management-frontend/static/js/main.adc03c47.chunk.js"
+  },
+  {
+    "revision": "e9a33e7ecbdc8d07dac7",
+    "url": "https://MrZzard.github.io/task-management-frontend/static/js/runtime~main.cbbf3476.js"
+  }
+]);
